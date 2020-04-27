b8968e082ebcd4c4e81a08babec3b41e71626fe0
Key5771 <ksj57715@hanmail.net>
2020-04-27 13:45:09 +0900

    포털서비스 개발론(4)-3 dao를 사용할 때, 가끔씩 서버가 죽는경우 해결

--- UserDao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ UserDao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -10,45 +10,82 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public User get(Integer id) throws ClassNotFoundException, SQLException {
-        // mysql
-        // driver 로딩
-        Connection connection = dataSource.getConnection();
-        // query
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("select id, name, password from userinfo where id = ?");
-        preparedStatement.setInt(1, id);
-        // 실행
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        // 결과매핑
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        // 자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+    public User get(Integer id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
+        try {
+            // mysql
+            // driver 로딩
+            connection = dataSource.getConnection();
+            // query
+            preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
+            preparedStatement.setInt(1, id);
+            // 실행
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            // 결과매핑
+            user = new User();
+            user.setId(resultSet.getInt("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            // 자원해지
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         // 결과리턴
         return user;
     }
 
-    public void insert(User user) throws ClassNotFoundException, SQLException {
-        // mysql
-        // driver 로딩
-        Connection connection = dataSource.getConnection();
-        // query
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)",  Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.executeUpdate();
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-        user.setId(resultSet.getInt(1));
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+    public void insert(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            // mysql
+            // driver 로딩
+            connection = dataSource.getConnection();
+            // query
+            preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)",  Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getInt(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
