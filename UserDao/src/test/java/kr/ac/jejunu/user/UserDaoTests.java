90e8228ec30dba53fd2bfdf6e5579ce28bed92ae
Key5771 <ksj57715@hanmail.net>
2020-04-27 13:17:02 +0900

    포털서비스 개발론(4)-1

--- UserDao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ UserDao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -1,6 +1,9 @@
 package kr.ac.jejunu.user;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -12,6 +15,14 @@ public class UserDaoTests {
     String name = "key";
     String password = "1234";
 
+    private static UserDao userDao;
+
+    @BeforeAll
+    public static void setup() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
+
     // 제주대학교
     @Test
     public void get() throws SQLException, ClassNotFoundException {
@@ -19,8 +30,7 @@ public class UserDaoTests {
 
 //        ConnectionMaker connectionMaker = new JejuConnectionMaker();
 //        UserDao userDao = new UserDao(connectionMaker);
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
+
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -36,8 +46,7 @@ public class UserDaoTests {
 //        ConnectionMaker connectionMaker = new JejuConnectionMaker();
 //        UserDao userDao = new UserDao(connectionMaker);
 
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
+
         userDao.insert(user);
         assertThat(user.getId(), greaterThan(0));
 
