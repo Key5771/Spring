c0c8148aaa9ca62119a3c6e39dd964588aa042aa
Key5771 <ksj57715@hanmail.net>
2020-05-10 16:48:44 +0900

    Connection을 상품정보에서도 사용할 수 있도록 변경(#3) Create ConnectionMaker

--- UserManagement/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ UserManagement/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -2,9 +2,11 @@ package kr.ac.jejunu.user;
 
 import java.sql.*;
 
-public abstract class UserDao {
+public class UserDao {
+    private final ConnectionMaker connectionMaker = new JejuConnectionMaker();
+
     public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -25,7 +27,7 @@ public abstract class UserDao {
     }
 
     public void insert(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
@@ -38,10 +40,4 @@ public abstract class UserDao {
         preparedStatement.close();
         connection.close();
     }
-
-    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=UTC"
-//                , "jeju", "jejupw");
-
 }
--- UserManagement/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ UserManagement/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -16,7 +17,7 @@ public class UserDaoTests {
     public void testGet() throws SQLException, ClassNotFoundException {
         Integer id = 1;
 
-        UserDao userDao = new JejuUserDao();
+        UserDao userDao = new UserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -28,8 +29,9 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
 
-        UserDao userDao = new JejuUserDao();
+        UserDao userDao = new UserDao();
         userDao.insert(user);
+        assertThat(user.getId(), greaterThan(0));
 
         User insertedUser = userDao.get(user.getId());
         assertThat(insertedUser.getName(), is(user.getName()));
@@ -42,7 +44,7 @@ public class UserDaoTests {
     public void testGetHalla() throws SQLException, ClassNotFoundException {
         Integer id = 1;
 
-        UserDao userDao = new HallaUserDao();
+        UserDao userDao = new UserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -54,8 +56,9 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
 
-        UserDao userDao = new HallaUserDao();
+        UserDao userDao = new UserDao();
         userDao.insert(user);
+        assertThat(user.getId(), greaterThan(0));
 
         User insertedUser = userDao.get(user.getId());
         assertThat(insertedUser.getName(), is(user.getName()));
