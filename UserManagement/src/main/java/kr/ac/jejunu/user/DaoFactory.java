e6f0159db06a49c23c932db5ffd1f3d71dd1816d
Key5771 <ksj57715@hanmail.net>
2020-05-10 17:38:50 +0900

    DataSource 구현(#6)

--- UserManagement/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ UserManagement/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -3,16 +3,34 @@ package kr.ac.jejunu.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
-    @Bean // new해주는 instance를 Spring이 해주는 역할
+    private String className;
+    private String url;
+    private String userName;
+    private String password;
+
+    @Bean // new해주는 instance를 Spring이 해주는 역
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        return new UserDao(dataSource());
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
