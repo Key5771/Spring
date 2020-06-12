bd5b3030f6d4b43318310d8102072a3ebd5a7eb4
key5771 <ksj57715@hanmail.net>
2020-06-12 15:14:04 +0900

    HttpSession을 이용한 쿠키 저장(포털서비스 개발론 11-1)

--- UserDao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ UserDao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -2,25 +2,27 @@ package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.*;
 
 @Controller
-@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
 
-    @RequestMapping("/user")
-    public User getUser(@RequestParam("id") Integer id) {
-        return userDao.get(id);
+    @RequestMapping(path = "/user")
+    public User getUser(@RequestParam("id") Integer id, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if(user == null)
+            user = userDao.get(id);
+        session.setAttribute("user", User.builder().name("Session").build());
+        return user;
     }
 
     @RequestMapping("/exception")
@@ -28,12 +30,12 @@ public class UserController {
         throw new RuntimeException("어이쿠!");
     }
 
-    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    @GetMapping("/upload")
     public void upload() {
 
     }
 
-    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    @PostMapping("/upload")
     public ModelAndView upload(@RequestParam("file")MultipartFile file, HttpServletRequest request) throws IOException {
         File path = new File(request.getServletContext().getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
         FileOutputStream fileOutputStream = new FileOutputStream(path);
